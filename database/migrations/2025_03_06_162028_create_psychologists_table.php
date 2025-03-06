ddd48ecc57cd0c898da63e8c395e0911cd628d4d
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-03-06 13:25:36 -0300

    feat: creating migrations

---
+++ database/migrations/2025_03_06_162028_create_psychologists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('psychologists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('person_id')->constrained();
+            $table->string('license_number')->unique();
+            $table->string('approach');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('psychologists', function (Blueprint $table) {
+            $table->dropForeign(['person_id']);
+        });
+        Schema::dropIfExists('psychologists');
+    }
+};
---
+++ database/migrations/2025_03_06_162039_create_schedules_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * - Schedules
+     * - id
+     * - date
+     * - start_time
+     * - end_time
+     * - psychologist_id
+     * - patient_id
+     * - created_at
+     * - updated_at
+     * - deleted_at
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->foreignId('psychologist_id')->constrained();
+            $table->foreignId('patient_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
