01f2febcc2aab31be01e5cf0ae5e9fd22c365e03
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-03-06 19:53:44 -0300

    feat: creating model role and RoleSeeder implements

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -9,9 +10,33 @@ class RoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * - admin
+     * - manager
+     * - psychologist
+     * - patient
+     * - secretary
      */
     public function run(): void
     {
-        //
+        Role::created([
+            'name' => 'Admin',
+            'description' => 'Administrador do sistema',
+        ]);
+        Role::created([
+            'name' => 'Manager',
+            'description' => 'Gerente do sistema',
+        ]);
+        Role::created([
+            'name' => 'Psychologist',
+            'description' => 'Psicólogo',
+        ]);
+        Role::created([
+            'name' => 'Patient',
+            'description' => 'Paciente',
+        ]);
+        Role::created([
+            'name' => 'Secretary',
+            'description' => 'Secretário',
+        ]);
     }
 }
