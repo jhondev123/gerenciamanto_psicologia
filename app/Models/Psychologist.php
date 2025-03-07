7588cb5a4666466e53be6cb7156fa5edd87628f5
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-03-06 22:51:34 -0300

    feat: creating models

---
+++ app/Models/Psychologist.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Psychologist extends Model
+{
+    /** @use HasFactory<\Database\Factories\PsychologistFactory> */
+    use HasFactory;
+}
---
+++ app/Models/ReportTemplate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReportTemplate extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReportTemplateFactory> */
+    use HasFactory;
+}
