aa9e1a786ff7b520cadb740b9956cf91e6afe479
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-03-06 22:40:50 -0300

    feat: creating routes to authentication and creating controller with methods not implemented

---
+++ app/Http/Controllers/V1/Auth/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\V1\Auth;
+
+use Nette\NotImplementedException;
+
+class AuthController
+{
+    public function login()
+    {
+        throw new NotImplementedException();
+    }
+
+    public function register()
+    {
+        throw new NotImplementedException();
+
+    }
+
+    public function logout()
+    {
+        throw new NotImplementedException();
+
+    }
+
+    public function me()
+    {
+        throw new NotImplementedException();
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\V1\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [AuthController::class, 'login'])->name('v1.auth.login');
+        Route::post('/register', [AuthController::class, 'register'])->name('v1.auth.register');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('v1.auth.logout');
+        Route::post('/me', [AuthController::class, 'me'])->name('v1.auth.me');
+    });
+});
