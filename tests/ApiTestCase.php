b0f8a4584e156341b746fe9fc3d307be250092c0
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-03-10 19:20:14 -0300

    feat: configuring test case from tests

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -2,7 +2,18 @@
 
 namespace Tests;
 
-class ApiTestCase
-{
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
+abstract class ApiTestCase extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withHeaders([
+            'Accept' => 'application/json',
+        ]);
+    }
 }
